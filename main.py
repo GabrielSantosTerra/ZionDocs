a4840e7d13b5e98c1afe6c6b0c3f60277ab75a8e
Gabriel Terra <gabrielsantosterra@gmail.com>
2025-07-17 14:31:39 -0300

    feat: liberado cors no backend

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware, FastAPI
 from app.routes import docs_routes as documentos
 from app.routes import user_routes as usuarios
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://docrh-615cf.web.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(usuarios.router, prefix="/users", tags=["Usuários"])
 app.include_router(documentos.router, prefix="/documents", tags=["Documentos"])
